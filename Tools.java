7fa0db5185fce061b2939291e5c83948b0b7cb78
Michail-Panagiotis Bofos <49307193+mbofos01@users.noreply.github.com>
2021-11-04 20:17:34 +0200

    plot work

--- Tools.java
+++ Tools.java
@@ -1,6 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Tools {
 	/**
@@ -39,6 +43,97 @@ public class Tools {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * This function is used to write a few lines to a file.
+	 * 
+	 * @param filename The name of the file in which we want to write
+	 * @param list     The String lines we print to the file
+	 */
+	public static void writeFile(String filename, ArrayList<String> list) {
+
+		try {
+			FileWriter fw = new FileWriter(filename, false);
+			for (String s : list) {
+				fw.write(s + "\n");// appends the string to the file
+			}
+			fw.close();
+		} catch (IOException ioe) {
+			System.err.println("IOException: " + ioe.getMessage());
+		}
+	}
+
+	/**
+	 * This function counts all the appearances of each letter and returns all the
+	 * lines in an arraylist.
+	 * 
+	 * @param alpha    Integer array that contains the appearances of each letter
+	 * @param filename The name of the file we use
+	 * @return Arraylist of strings - each line is a member of the arraylist
+	 */
+	public static ArrayList<String> countLetters(int[] alpha, String filename) {
+		int cnt = 0;
+		ArrayList<String> lets = new ArrayList<>();
+		try {
+			File myObj = new File(filename);
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				cnt++;
+				lets.add(myReader.nextLine());
+
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		for (String a : lets)
+			alpha[a.charAt(0) - 'A']++;
+
+		return lets;
+	}
+
+	/**
+	 * This function creates the training and testing set of the letter recognition
+	 * problem. First we count all the letters used ( A 700 B 720 ...) and then we
+	 * add 70% of each letter to the training set and the rest 30% to the testing
+	 * set (After we shuffle our sets a hundred times in order to create a set we
+	 * can use without it becoming biased).
+	 * 
+	 * @param args The name of the file which contains all of your (Normalized)
+	 *             data.
+	 */
+	public static void createTrainAndTestSets(String args) {
+		int[] alpha = new int[26];
+		ArrayList<String> lets = countLetters(alpha, args);
+		ArrayList<ArrayList<String>> letterlines = new ArrayList<>();
+		for (int i = 0; i < 26; i++)
+			letterlines.add(new ArrayList<String>());
+
+		Collections.sort(lets);
+		for (String a : lets)
+			letterlines.get((int) (a.charAt(0) - 'A')).add(a);
+
+		for (int i = 0; i < 26; i++) {
+			Collections.shuffle(letterlines.get(i));
+		}
+
+		ArrayList<String> train = new ArrayList<>();
+		ArrayList<String> test = new ArrayList<>();
+		for (int i = 0; i < 26; i++) {
+			int j;
+			for (j = 0; j < letterlines.get(i).size() * 0.7; j++)
+				train.add(letterlines.get(i).get(j));
+			for (; j < letterlines.get(i).size(); j++)
+				test.add(letterlines.get(i).get(j));
+		}
+		for (int i = 0; i < 100; i++) {
+			Collections.shuffle(train);
+			Collections.shuffle(test);
+		}
+		writeFile("train.txt", train);
+		writeFile("test.txt", test);
 	}
 
 	public static double Euclidean(Node winner, Node random_node) {
---
+++ map.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import sys
+
+x =  [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+y = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+letters = []
+
+markers = ["$A$","$B$","$C$","$D$","$E$","$F$","$G$","$H$","$I$","$J$","$K$","$L$",
+"$M$","$N$","$O$","$P$","$Q$","$R$","$S$","$T$","$U$","$V$","$W$","$X$","$Y$","$Z$"]
+
+with open(sys.argv[1], "r") as reader:
+    lines = reader.readlines()
+  
+
+f=open("data.txt","r") 
+for line in f:
+    a = line.strip('\n')
+    b = a.split(' ')
+    place = ord(b[0]) - ord('A')
+    x[place].append(float(b[1]))
+    y[place].append(float(b[2]))
+
+maxesX = []
+for i in range(26):    
+    if(len(x[i]) != 0):
+        maxesX.append(max(x[i]))
+maxesY = []
+for i in range(26):    
+    if(len(y[i]) != 0):
+        maxesY.append(max(y[i]))
+
+x_max = max(maxesX)  + 0.2*max(maxesX)
+y_max = max(maxesY) + 0.2*max(maxesY)
+
+fig = plt.figure()
+
+plt.Axes.set_frame_on
+plt.title("Error - Epoch")
+for i in range(26):
+    enumerate(markers)
+    plt.plot(x[i],y[i], marker=markers[i],lw=0)
+
+
+plt.ylabel('error')
+plt.xlabel('epoch')
+plt.grid(True)
+plt.xlim([0, x_max])
+plt.ylim([0, y_max])
+plt.show()
